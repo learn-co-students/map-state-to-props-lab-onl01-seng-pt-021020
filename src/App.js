25d6ce4a8918bd126cdb4002c17ef670331d8c75
08fdange <08fdange@gmail.com>
2020-10-28 00:58:45 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import UserInput from './components/UserInput'
-import ConnectedUsers from './components/Users'
+import UserInput from './components/UserInput';
+import ConnectedUsers from './components/Users';
+// import Users from './components/Users';
+
 
 class App extends Component {
   render() {
