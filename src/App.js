5e1eae3878b1f7bff622ddf288302ca72ba1dabd
jkatem <jenmiao@gmail.com>
2020-11-01 11:34:04 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import UserInput from './components/UserInput'
 import ConnectedUsers from './components/Users'
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
+
         <UserInput />
         <ConnectedUsers />
       </div>
     );
   }
 }
+ export default App
 
-export default App;
